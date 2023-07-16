b19bd90a6a46bbe8e34a6647a2f43afb2787cc42
dataKernel <lancelot.saintleger@gmail.com>
2023-07-16 13:14:35 +0100

    training exam submit first_word exercise

---
+++ exam_training/first_word.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+//./a.out toto est la
+int     main(int argc, char *argv[])
+{
+    int     i;
+    int     j;
+
+    i = 1;
+    if(argc != 2)
+    {
+        ft_putchar('\n');
+        return(0);
+    }
+    while(i < 2)
+    {
+        j = 0;
+        while(argv[i][j])
+        {
+            if(argv[i][j] == ' ')
+            {
+                ft_putchar('\n');
+                return(0);
+            }
+            ft_putchar(argv[i][j]);
+            j++;
+        }
+        i++;
+    }
+    ft_putchar('\n');
+    return(0);
+}
