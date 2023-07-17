ad1de5141882d890027a499c6469790645ed6033
dataKernel <lancelot.saintleger@gmail.com>
2023-07-17 18:33:02 +0100

    fixing the func with array testing -> strcpy.c

--- exam_training/strcpy.c
+++ exam_training/strcpy.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 
-void    ft_strcpy(char *dst, char *src)
+char    *ft_strcpy(char *dst, const char *src)
 {
     int     i;
 
     i = 0;
     if(src == NULL)
-        return;
+        return dst;
     while(src[i])
     {
         dst[i] = src[i];
         i++;
     }
     dst[i] = '\0';
+    return(dst);
 }
 
 int     main(void)
 {
-    char    dst[10];
-
-    ft_strcpy(dst, "toto");
+    char    dst[42];
+    ft_strcpy(dst, "42 is a school");
+    printf("%s", dst);
     return(0);
 }
