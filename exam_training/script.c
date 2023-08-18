25837d144c784b479216db8c5a089929df34a616
dataKernel <lancelot.saintleger@gmail.com>
2023-08-18 17:42:32 +0100

    ff

--- exam_training/script.c
+++ exam_training/script.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
-#include <unistd.h>
 
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
-void    ft_putstr(char *str)
+void    ft_draw_square(int size)
 {
     int     i;
+    int     j;
 
     i = 0;
-    while(str[i])
+    while(i < size)
     {
-        ft_putchar(str[i]);
+        j = 0;
+        while(j < size)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
         i++;
     }
 }
 
-int     ft_strlen(char *str)
+int     ft_atoi(char *str)
 {
+    int     result;
     int     i;
 
+    result = 0;
     i = 0;
-    while(str[i])
+    while(str[i] < '0' || str[i] > '9')
         i++;
-    return(i);
+    while(str[i])
+    {
+        result = result * 10 + str[i] - '0';
+        i++;
+    }
+    return(result);
 }
 
 int     main(int argc, char **argv)
 {
-    int     i;
-    int     j;
-    int     k;
+    int     argument;
 
-    if(argc != 3)
-    {
-        ft_putchar('\n');
-        return(0);
-    }
-
-    
+    argument = ft_atoi(argv[1]);
+    ft_draw_square(argument);
     return(0);
 }
