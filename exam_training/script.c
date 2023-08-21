295b648bdddc474ae6d4f28d0337cdf883068316
dataKernel <lancelot.saintleger@gmail.com>
2023-08-21 16:07:11 +0100

    fixing

--- exam_training/script.c
+++ exam_training/script.c
@@ -1,67 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-typedef struct s_point
+void	ft_putchar(char c)
 {
-    int     x;
-    int     y;
-}t_point;
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-void    ft_putstr(char *str)
-{
-    int     i;
-    while(str[i])
-    {
-        ft_putchar(str[i]);
-        i++;
-    }
-}
-char    **make_area(char **zone, t_point size)
-{
-    char **new;
-
-    new = malloc(sizeof(char *) * size.y);
-    for (int i = 0; i < size.y; i++)
-    {
-        new[i] = malloc(size.x + 1);
-        for (int j = 0; j < size.x; j++)
-            new[i][j] = zone[i][j];
-        new[i][size.x] = '\0';
-    }
-
-    return new;
+	write(1, &c, 1);
 }
 
-void    flood_fill(char **tab, t_point size, t_point begin)
+void	ft_putstr(char *str)
 {
-    //...
+	int		i;
+
+	i = 0;
+	while(str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
 }
 
-int     main(void)
+int		ft_strlen(char *str)
 {
-    t_point     size = {4, 4};
-    char        **area;
-    char        *zone[]= 
-    {
-        "11111111",
-        "10001001",
-        "10010001",
-        "10110001",
-        "11100001",
-    };
+	int		i;
 
-    area = make_area(zone, size);
-    for(int i = 0; i <size.y; i++)
-        printf("%s\n", area[i]);
-    return(0);
+	i = 0;
+	while(str[i])
+		i++;
+	return(i);
 }
 
-
-
-
-
-
+int		main(void)
+{
+	return(0);
+}
