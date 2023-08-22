96af3005b2da803ca23812965d8109294ef5b37f
dataKernel <lancelot.saintleger@gmail.com>
2023-08-22 16:49:11 +0100

    ff

--- exam_training/script.c
+++ exam_training/script.c
@@ -1,77 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-void	ft_putstr(char *str)
-{
-	int		i;
-
-	i = 0;
-	while(str[i])
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
-int		ft_strlen(char *str)
-{
-	int		i;
-
-	i = 0;
-	while(str[i])
-		i++;
-	return(i);
-}
-
-int		check_size_nbr(int nbr)
-{
-	int		res;
-
-	res = 0;
-	if(nbr < 0)
-		res ++;
-	while(nbr > 0 || nbr < 0)
-	{
-		nbr /= 10;
-		res++;
-	}
-	return(res);
-}
-
-char	*ft_itoa(int nbr)
-{
-	int		i;
-	int		sizeNbr;
-	char	*string;
-	
-	string = NULL;
-	sizeNbr = check_size_nbr(nbr);
-	i = sizeNbr - 1;
-	string = (char *)malloc(sizeof(char) * (sizeNbr + 1));
-	string[i + 1] = '\0';
-	if(nbr < 0)
-	{
-		nbr *= -1;
-		string[0] = '-';
-	}
-	while(nbr > 0 || nbr < 0)
-	{
-		string[i] = nbr % 10 + '0';
-		nbr /= 10;
-		i--;
-	}
-	return(string);
-}
 
 int		main(void)
 {
-	char	*test;
-
-	test = ft_itoa(-879898);
-	printf("value --> %s\n", test);
 	return(0);
 }
