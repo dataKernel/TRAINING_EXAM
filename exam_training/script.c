06259dfb0764c9e4b9acbb474e40d4cfe00d1df3
dataKernel <lancelot.saintleger@gmail.com>
2023-08-23 14:31:19 +0100

    ff

--- exam_training/script.c
+++ exam_training/script.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int		ft_atoi_base(const char *str, int str_base)
-{
-	int		index;
-
-	
-	index = 0;
-}
-
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
+int		ft_atoi_base(const char *str, int strBase)
+{
+	const char	*baseFixed;
+	int			res;
+	int			i;
+
+	baseFixed = "0123456789abcdef";
+	res = 0;
+	i = 0;
+	while(i < strBase)
+	{
+		printf("%c__", baseFixed[i]);
+		i++;
+	}
+	return(res);
+}
+
+
 int		main(void)
 {
+	int		n;
+
+	n = ft_atoi_base("ff", 10);
 	return(0);
 }
