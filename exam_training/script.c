447cec0e5038ca2de51ad39cc1dd262dcbeb7f66
dataKernel <lancelot.saintleger@gmail.com>
2023-11-20 07:54:18 +0000

    review exam 42

--- exam_training/script.c
+++ exam_training/script.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void    ft_utlitmate_div_mod(int *a, int *b)
+void    ft_ultitmate_div_mod(int *a, int *b)
 {
     int     temp;
 
@@ -9,14 +9,37 @@ void    ft_utlitmate_div_mod(int *a, int *b)
     *b = temp % *b;
 }
 
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return(i);
+}
+
+void    ft_rev_tab(int *tab, int size)
+{
+    int     i;
+
+    i = 0;
+    
+}
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
 int     main(void)
 {
+    int     tab[5] = {12 -4, 0, 1, 32};
     int     a;
     int     b;
 
     a = 10;
     b = 2;
-    ft_utlitmate_div_mod(&a, &b);
-    printf("a:%i__b:%i", a, b);
+    ft_ultitmate_div_mod(&a, &b);
     return(0);
 }
