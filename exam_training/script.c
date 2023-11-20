eed9fb448997750248b3f46367dc9ca030b5e057
dataKernel <lancelot.saintleger@gmail.com>
2023-11-20 08:28:28 +0000

    review exam 42

--- exam_training/script.c
+++ exam_training/script.c
@@ -39,6 +39,30 @@ void    ft_rev_tab(int *tab, int size)
     }
 }
 
+void    ft_sort_int_tab(int *tab, int size)
+{
+    int     temp;
+    int     i;
+    int     j;
+
+    i = 0;
+    while(i < size)
+    {
+        j = i + 1;
+        while(j < size)
+        {
+            if(tab[j] < tab[i])
+            {
+                temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+            }
+            j++;
+        }
+        i++;
+    }   
+}
+
 int     main(void)
 {
     int     tab[SIZE_TAB] = {12, -4, 0, 1, 32};
@@ -47,7 +71,7 @@ int     main(void)
     for(int i = 0; i < SIZE_TAB; i++)
         printf("tab[%i]->%i__", i, tab[i]);
     printf("\n");
-    ft_rev_tab(tab, SIZE_TAB);
+    ft_sort_int_tab(tab, SIZE_TAB);
     for(int i = 0; i < SIZE_TAB; i++)
         printf("tab[%i]->%i__", i, tab[i]);
     return(0);
