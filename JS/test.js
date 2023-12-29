f008c05063f5628e264f15a38c7bb36024620945
dataKernel <lancelot.saintleger@gmail.com>
2023-12-29 11:15:24 +0000

    properties/members protection inside object definition -> exemple showing why non mutable properties should not be changed

--- JS/test.js
+++ JS/test.js
@@ -1,22 +1,26 @@
+// Instructions
+// Checkpoint 1 Enabled
 // 1.
-// Let’s create a new object to practice using this.
-// In main.js there is an object robot, add a property of model and assign to it a value of '1E78V2'. Add another property, energyLevel and assign to it a value of 100.
-
+// Below the robot object, reassign the _energyLevel property to 'high'.
 // 2.
-// Inside the robot object, add a method named provideInfo. Inside the body of provideInfo(), return the following string by using interpolation:
-// I am MODEL and my current energy level is ENERGYLEVEL.  
-// Replace ‘MODEL’ and ‘ENERGYLEVEL’ with the calling object’s model and energyLevel property. Remember, to get the access to the calling object’s properties inside a method, you have to use the this keyword!
-
-// 3.
-// Now to check .provideInfo() has access to the internal properties of robot. Log the result of calling .provideInfo() method on robot to the console.
+// Now take a look at the new recharge method in robot. .recharge() will add 30 
+//to _energyLevel.
+// What will happen now that _energyLevel isn’t a number?
+// Call .recharge() on robot to find out.
+// Notice that a funky string is printed to the console! 
+//This is known as a side-effect of type-coercion. No need to worry about 
+//what this means for now, but it’s important to understand that you can cause 
+//unwanted side-effects when mutating objects and their properties.
 
 const robot = {
-	model: "1E78V2",
-	energyLevel: 100,
-	provideInfo()
-	{
-		let str = `I am ${this.model} and my current energy is ${this.energyLevel}`;
-		return(str);
-	}
+  _energyLevel: 100,
+  recharge()
+  {
+    this._energyLevel += 30;
+    console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
+  }
 };
-console.log(robot.provideInfo());
+
+robot._energyLevel = "high";
+robot.recharge();
+console.log(robot);
