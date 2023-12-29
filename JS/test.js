bd0e853e5914b1567081af974febd4397846b7d4
dataKernel <lancelot.saintleger@gmail.com>
2023-12-29 15:31:06 +0000

    test

--- JS/test.js
+++ JS/test.js
@@ -12,5 +12,24 @@ const		obj =
 	},
 };
 
+const	obj2 =
+{
+	//dangereux il faudra tjs set les membres avant de call la methode
+	x: undefined,
+	y: undefined,
+
+	calcul_aire()
+	{
+		let		res;
+
+		res = this.x * this.y;
+		return(res);
+	}
+}
+
 let		aire_rectangle = obj.calcul_aire();
+obj2.x = 2;
+obj2.y = 3;
+console.log(obj2);
+aire_rectangle = obj2.calcul_aire();
 console.log(aire_rectangle);
