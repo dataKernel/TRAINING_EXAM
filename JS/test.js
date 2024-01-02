b032ca53abc0d19c663a2c7c4bc0a9d23009bc60
dataKernel <lancelot.saintleger@gmail.com>
2024-01-02 17:55:13 +0000

    destructured assignement test

--- JS/test.js
+++ JS/test.js
@@ -1,43 +1,23 @@
-// Instead of making individual robots like we’ve been doing, let’s make a factory 
-//function to make robots as we please!
-// Create a factory function named robotFactory that has two parameters model and 
-//mobile. Make the function return an object.In the object, add a key of model with 
-//the value of the model parameter.Add another property that has a key of mobile with 
-//a value of the mobile parameter.
-// Then add a method named beep without a parameter that will log 'Beep Boop' to the console.
+// 1.
+// Use destructured assignment to create a const variable named functionality that extracts the 
+//functionality property of robot.
+// If you need a reminder on how to use destructured assignment, review the example in the narrative 
+//or check the hint.
 // 2.
-// Use your factory function by declaring a const variable named tinCan.Assign 
-//to tinCan the value of calling robotFactory with the first argument of 
-//'P-500' and the second argument of true.
-// 3.
-// Let’s now check what tinCan can do !Call.beep() on tinCan.
-// You should see 'Beep Boop' printed to the console which means the factory 
-//function produced a robot object! Play around with tinCan 
-//to check the other properties!
+// Since functionality is referencing robot.functionality we can call the methods available to 
+//robot.functionality simply through functionality.
+// Take advantage of this shortcut and call the.beep() method on functionality.
 
-const	factory = (x, y) =>
+const robot = 
 {
-	const	obj =
+	model: '1E78V2',
+	energyLevel: 100,
+	functionality: 
 	{
-		x: x,
-		y: y,
-		dire(){console.log("ttt")}
-	};
-	return(obj);
-};
-
-const	obj = 
-{
-	x: 12,
-	y: 11,
-	dire()
-	{
-		console.log("ttt");
+		beep() {console.log('Beep Boop');},
+		fireLaser() {console.log('Pew Pew');},
 	}
 };
 
-const	test = (varia) =>
-	varia + 1;
-
-console.log(obj);
-console.log(factory(100, 50));
+const	{functionality} = robot;
+functionality.beep();
