1e41b8e3e576958e12ac4550198de59167dd9d71
dataKernel <lancelot.saintleger@gmail.com>
2024-01-03 15:07:33 +0000

    testing Object class and methods such as keys() and assign()

--- JS/test.js
+++ JS/test.js
@@ -1,23 +1,48 @@
 // 1.
-// Use destructured assignment to create a const variable named functionality that extracts the 
-//functionality property of robot.
-// If you need a reminder on how to use destructured assignment, review the example in the narrative 
-//or check the hint.
+// In main.js there is an object, robot.We’d like to grab the property names, otherwise known as keys, 
+//and save the keys in an array which is assigned to robotKeys.However, there’s something missing 
+//in the method call.
+// Find out what we have to include by reading MDN’s Object.keys() documentation.
+// Checkpoint 2 Step instruction is unavailable until previous steps are completed
 // 2.
-// Since functionality is referencing robot.functionality we can call the methods available to 
-//robot.functionality simply through functionality.
-// Take advantage of this shortcut and call the.beep() method on functionality.
+// Object.entries() will also return an array, but the array will contain more arrays that have both 
+//the key and value of the properties in an object.
+// Declare a const variable named robotEntries and assign to it the entries of robot by calling 
+//Object.entries().
+// To find how to use Object.entries(), read the documentation at MDN.
+// 3.
+// Now what if we want another object that has the properties of robot but with a few additional 
+// properties.Object.assign() sounds like a great method to use, but like the previous examples we should check 
+// Object.assign() documentation at MDN.
+// Declare a const variable named newRobot.newRobot will be a new object that has all the properties 
+//of robot and the properties in the following object: { laserBlaster: true, voiceRecognition: true }. 
+//Make sure that you are not changing the robot object!
 
-const robot = 
-{
-	model: '1E78V2',
-	energyLevel: 100,
-	functionality: 
-	{
-		beep() {console.log('Beep Boop');},
-		fireLaser() {console.log('Pew Pew');},
-	}
+const robot = {
+	model: 'SAL-1000',
+	mobile: true,
+	sentient: false,
+	armor: 'Steel-plated',
+	energyLevel: 75
 };
 
-const	{functionality} = robot;
-functionality.beep();
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// // Declare robotEntries below this line:
+const	robotEntries = Object.entries(robot);
+
+// console.log(robotEntries);
+console.log(robotEntries);
+// // Declare newRobot below this line:
+const	newRobot =
+{
+	laserBlaster: true,
+	voiceRecognition: true
+};
+Object.assign(newRobot, robot);
+
+// console.log(newRobot);
+console.log(newRobot);
