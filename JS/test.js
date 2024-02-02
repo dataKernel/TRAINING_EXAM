d246a27abe51ac02ae9b9794cef381162589b3c2
dataKernel <lancelot.saintleger@gmail.com>
2024-02-02 18:02:57 +0000

    experimenting a checker mail function with native JS algo

--- JS/test.js
+++ JS/test.js
@@ -1,28 +1,61 @@
-const robot = {
-	model: 'SAL-1000',
-	mobile: true,
-	sentient: false,
-	armor: 'Steel-plated',
-	energyLevel: 75
-};
-Object.valueof
-// What is missing in the following method call?
-const robotKeys = Object.keys(robot);
+const 	add = function(a, b){console.log(`res: ${a + b}`); return(a + b);}
+const	addArrow = (a, b) => {return(a + b);};
+const	addArrow2 = (a, b) => a + b;
 
-console.log(robotKeys);
+console.log(add(1, 1));
+console.log(addArrow(2, 2));
+console.log(addArrow2(3, 3));
 
-// // Declare robotEntries below this line:
-const	robotEntries = Object.entries(robot);
-
-// console.log(robotEntries);
-console.log(robotEntries);
-// // Declare newRobot below this line:
-const	newRobot =
+function 	check_string_equals(str1, str2)
 {
-	laserBlaster: true,
-	voiceRecognition: true
-};
-Object.assign(newRobot, robot);
+	let		check;
+	let		i;
 
-// console.log(newRobot);
-console.log(newRobot);
+	if (typeof (str1) != "string" || typeof (str2) != "string")
+		return (false);
+	i = 0;
+	check = true;
+	while(str1[i] || str2[i])
+	{
+		if(str1[i] != str2[i])
+		{
+			check = false;
+			break;
+		}
+		i++;
+	}
+	return(check);
+}
+
+function 	check_email_is_valid(email)
+{
+	const	tabDns = ["gmail", "hotmail", "outlook", "gm"];
+	let		check = false;
+	let		dns = [];
+	let		i = 0;
+
+	while(i < email.length)
+	{
+		if(email[i] == '@')
+		{
+			check = true;
+			i++;
+			while(email[i] != '.')
+			{
+				dns += email[i];
+				i++;
+			}
+		}
+		i++;
+	}
+	i = 0;
+	while(i < tabDns.length)
+	{
+		if(check_string_equals(dns, tabDns[i]) && check)
+			return(true);
+		i++;
+	}
+	return(false);
+}
+
+console.log(check_email_is_valid("la@gm.com"));
