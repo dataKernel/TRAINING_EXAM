a98be9830a1b5c3d4be7fd5457045276654ba3b6
dataKernel <lancelot.saintleger@gmail.com>
2024-02-26 15:42:56 +0100

    reviewing C basics

--- script.c
+++ script.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
-#include <unistd.h>
 
-void    ft_putchar(char c)
+int     ft_strlen(char *str)
 {
-    write(1, &c, 1);
+    int     i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return(i);
+}
+//abcde = edcba
+
+char    *ft_strrev(char *str)
+{
+    int     i;
+    int     j;
+    char    tmp;
+
+    i = 0;
+    j = ft_strlen(str) - 1;
+    while(i < ft_strlen(str) / 2)
+    {
+        tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+        j--;
+        i++;
+    }
+    return(str);
 }
 
 int     main(void)
 {
-    for(int i = 0; i < 3; i++)
-        ft_putchar(i + '0');
+    char    t[10] = "abcde";
+    ft_strrev(t);
+    printf("str rev:%s", t);
     return(0);
 }
