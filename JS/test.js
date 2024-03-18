84b9b05a4fb785080c7071a88cd4200c04a66a29
dataKernel <lancelot.saintleger@gmail.com>
2024-03-18 18:25:02 +0100

    testing

--- JS/test.js
+++ JS/test.js
@@ -1,61 +1,16 @@
-const 	add = function(a, b){console.log(`res: ${a + b}`); return(a + b);}
-const	addArrow = (a, b) => {return(a + b);};
-const	addArrow2 = (a, b) => a + b;
-
-console.log(add(1, 1));
-console.log(addArrow(2, 2));
-console.log(addArrow2(3, 3));
-
-function 	check_string_equals(str1, str2)
+const   mainObject =
 {
-	let		check;
-	let		i;
+    mainArray:
+    {
+        a: 10,
+        b: 20,
+        info: "lvl1",
+        nestedArray_lvl1:
+        {
+            a: 100,
+            b: 200,
+            info: "lvl2"
+        }
+    }
+};
 
-	if (typeof (str1) != "string" || typeof (str2) != "string")
-		return (false);
-	i = 0;
-	check = true;
-	while(str1[i] || str2[i])
-	{
-		if(str1[i] != str2[i])
-		{
-			check = false;
-			break;
-		}
-		i++;
-	}
-	return(check);
-}
-
-function 	check_email_is_valid(email)
-{
-	const	tabDns = ["gmail", "hotmail", "outlook", "gm"];
-	let		check = false;
-	let		dns = [];
-	let		i = 0;
-
-	while(i < email.length)
-	{
-		if(email[i] == '@')
-		{
-			check = true;
-			i++;
-			while(email[i] != '.')
-			{
-				dns += email[i];
-				i++;
-			}
-		}
-		i++;
-	}
-	i = 0;
-	while(i < tabDns.length)
-	{
-		if(check_string_equals(dns, tabDns[i]) && check)
-			return(true);
-		i++;
-	}
-	return(false);
-}
-
-console.log(check_email_is_valid("la@gm.com"));
