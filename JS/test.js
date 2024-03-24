9918530af399f8c9b74ccc9f8c80a7f36bd5ac68
dataKernel <lancelot.saintleger@gmail.com>
2024-03-24 10:12:25 +0100

    review class js

--- JS/test.js
+++ JS/test.js
@@ -1,21 +1,24 @@
-//méthode classique
-const   afficher_nbr = (n) => 
-{
-    console.log("le nombre est " + n);
+class Dog {
+    constructor(name) {
+        this._name = name;
+        this._behavior = 0;
+    }
+
+    get name() {
+        return this._name;
+    }
+    get behavior() {
+        return this._behavior;
+    }
+
+    incrementBehavior() {
+        this._behavior++;
+    }
 }
-//méthode Factory
-function    usine_guerrier(nom, force, armure, arme)
-{
-    return{
-        nom: nom,
-        force: force,
-        armure: armure,
-        arme: arme
-    };
-}
-//generation de 3 guerriers différents (litteral object) à la volée
-const   guts = usine_guerrier("guts", 1000, 500, "epée géante");
-const   lancelot = usine_guerrier("lancelot", 300, 100, "épée de chevalier");
-const   ambre = usine_guerrier("ambre", 240, -40, "caipirinha");
-//checking de la gen d'objets via méthodes Factory
-console.log(guts, lancelot, ambre);
+
+const halley = new Dog('Halley');
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
+halley.incrementBehavior(); // Add one to behavior
+console.log(halley.name); // Print name value to console
+console.log(halley.behavior); // Print behavior value to console
