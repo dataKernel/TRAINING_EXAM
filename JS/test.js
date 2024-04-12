5848c99908bf8bf214dabde8fd2e6863a276d42c
dataKernel <lancelot.saintleger@gmail.com>
2024-04-12 13:27:02 +0200

    learning promise js

--- JS/test.js
+++ JS/test.js
@@ -1,24 +1,14 @@
-class Dog {
-    constructor(name) {
-        this._name = name;
-        this._behavior = 0;
-    }
+const inventory = {
+	sunglasses: 1900,
+	pants: 1088,
+	bags: 1344
+};
 
-    get name() {
-        return this._name;
-    }
-    get behavior() {
-        return this._behavior;
-    }
-
-    incrementBehavior() {
-        this._behavior++;
-    }
-}
-
-const halley = new Dog('Halley');
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
-halley.incrementBehavior(); // Add one to behavior
-console.log(halley.name); // Print name value to console
-console.log(halley.behavior); // Print behavior value to console
+// Write your code below:
+const 	myExecutor = function(resolve, reject)
+{
+	if(inventory.sunglasses > 0)
+		resolve("Sunglasses order processed");
+	else
+		reject("That item is solved out.");
+};
