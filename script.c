6c83c6d33d00d1daf57d43b6c2281ab922bc0c43
dataKernel <lancelot.saintleger@gmail.com>
2024-07-17 23:14:30 +0100

    review C

--- script.c
+++ script.c
@@ -1,18 +1,42 @@
 #include <stdio.h>
+#include <unistd.h>
 
-void    ft_div_mod(int a, int b, int *div, int *mod)
+void ft_putchar(char c)
 {
-    *div = a / b;
-    *mod = a % b;
+    write(1, &c, 1);
 }
 
-int     main(void)
+int ft_strlen(char *str)
 {
-    int     div = 0;
-    int     mod = 0;
+    int     i = 0;
 
-    printf("PRE(nbr): div_%i, mod_%i\n", div, mod);
-    ft_div_mod(4, 2, &div, &mod);
-    printf("POST(nbr): div_%i, mod_%i\n", div, mod);
-    return(0);
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char *ft_strrev(char *str)
+{
+    char              temp;
+
+    const int       sizeStr = ft_strlen(str);
+    int                 i = 0;
+    int                 j = sizeStr - 1;
+
+    while(i < sizeStr / 2)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++;
+        j--;
+    }
+    return(str);
+}
+
+int main(void)
+{
+    char    str[] = "abcde";
+    printf("%s", ft_strrev(str));
+    return (0);
 }
