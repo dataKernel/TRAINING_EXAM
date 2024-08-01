fdd5471d93cd7dcf08d5f5f257ec5ba322dda4bc
dataKernel <lancelot.saintleger@gmail.com>
2024-08-01 22:30:21 +0100

    .

--- script.c
+++ script.c
@@ -19,7 +19,7 @@ int		ft_iterative_power(int nb, int power)
 
 int		main(void)
 {
-	int		res = ft_iterative_power(4, 2);
+	int		res = ft_iterative_power(-4, 2);
 
 	printf("res: %i", res);
 	return(0);
