e7d63d68c67aa46c116db862bc4b7d19601af0ab
dataKernel <lancelot.saintleger@gmail.com>
2024-08-01 17:51:09 +0100

    daily fix

--- script.c
+++ script.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
-int     ft_recursive_factorial(int nb)
+int	ft_iterative_pow(int nb, int pow)
 {
-    if(nb == 0)
-        return(0);
-    if(nb == 1)
-        return(1);
-    return(nb * ft_recursive_factorial(nb - 1));
+	int	i;
+	int	res;
+
+	i = 0;
+	res = 1;
+	while (i < pow)
+	{
+		res_state printf("res_state:%i\n", res);
+		res *= nb;
+		i++;
+	}
+	return (res);
 }
 
-int     main(void)
+int	main(void)
 {
-    printf("res: %i", ft_recursive_factorial(4)); //24
-    return(0);
+	int check = ft_iterative_pow(3, 3);
+	printf("%i", check);
+	return (0);
 }
