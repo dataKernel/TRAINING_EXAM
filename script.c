e964fc97a50bc97e916bd75db248eb2112de69e5
dataKernel <lancelot.saintleger@gmail.com>
2024-08-02 17:49:35 +0100

    .

--- script.c
+++ script.c
@@ -1,26 +1,16 @@
 #include <stdio.h>
 
-int		ft_iterative_power(int nb, int power)
+int		ft_recursive(int nb, int power)
 {
-	int		i = 0;
-	int		res = 1;
-
 	if(power < 0)
 		return(0);
 	if(power == 0)
 		return(1);
-	while(i < power)
-	{
-		res *= nb;
-		i++;
-	}
-	return(res);
+	return(nb * ft_recursive(nb, power - 1));
 }
 
 int		main(void)
 {
-	int		res = ft_iterative_power(-4, 2);
-
-	printf("res: %i", res);
+	printf("val: %i", ft_recursive(4, -10));
 	return(0);
 }
