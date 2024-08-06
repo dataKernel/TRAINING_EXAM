9b96bc9a817e279a7271f010b42dcd52588a95ba
dataKernel <lancelot.saintleger@gmail.com>
2024-08-06 11:50:23 +0100

    fibo iterative checking res

--- script.c
+++ script.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
-int		ft_fibonacci_iterative(int index)
+int	ft_fibonacci_iterative(int index)
 {
-	int		i = 0;
-	int		res = 0;
-	int		index0 = 0;
-	int		index1 = 1;
+	int	i;
+	int	res;
+	int	index0;
+	int	index1;
 
-	if(index == 0 || index == -1)
-		return(0);
-	if(index == 1)
-		return(1);
-	//we know for sure know our calcul will start on index 2 as the min value
-	while(i <  index)
+	i = 0;
+	res = 0;
+	index0 = 0;
+	index1 = 1;
+	if (index == 0 || index == -1)
+		return (0);
+	if (index == 1)
+		return (1);
+	// we know for sure know our calcul will start on index 2 as the min value
+	while (i < index)
 	{
 		res = index0 + index1;
 		index0 = index1;
 		index1 = res;
 		i++;
 	}
-	return(res);
+	return (res);
 }
 
-int		main(void)
+int	main(void)
 {
-	printf("%i ", ft_fibonacci_iterative(-1));
-	printf("%i ", ft_fibonacci_iterative(0));
-	printf("%i ", ft_fibonacci_iterative(1));
-	printf("%i ", ft_fibonacci_iterative(5));
-	return(0);
+	for (int i = 0; i <= 5; i++)
+		printf("fib(%i):%i\n", i, ft_fibonacci_iterative(i));
+	return (0);
 }
