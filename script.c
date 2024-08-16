662ed9a1e1654edd63c21dd3cf42f4ea52e31340
dataKernel <lancelot.saintleger@gmail.com>
2024-08-16 12:46:35 +0100

    update arbo

--- script.c
+++ script.c
@@ -1,44 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   script.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: data_kernel <data_kernel@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 10:49:31 by data_kernel       #+#    #+#             */
+/*   Updated: 2024/08/16 10:56:37 by data_kernel      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
-
-void	ft_putchar(char);
-bool	ft_str_is_equal(const char*, const char*);
-char	*ft_strstr(char *, char *);
-
-bool	ft_str_is_equal(const char *s1, const char *s2)
-{
-	bool	check = true;
-	int		i = 0;
-
-	//DEBUG check infos
-	//printf("s1:%s  s2:%s\n", s1, s2);
-	while(s2[i])
-	{
-		if(s1[i] != s2[i])
-			return(false);
-		i++;
-	}
-	return(check);
-}
-
-char	*ft_strstr(char *str, char *toFind)
-{
-	int		i = 0;
-
-	while(str[i])
-	{
-		if(ft_str_is_equal(str + i, toFind))
-		{
-			return(str + i);
-		}
-		i++;
-	}
-	return(NULL);
-}
 
 int		main(void)
 {
-	printf("%s", ft_strstr("lancelot code dur..", "code"));
 	return(0);
 }
