023305dbb52cc61f9f0b59015414753911ea0299
dataKernel <lancelot.saintleger@gmail.com>
2024-08-17 19:22:59 +0100

    uppercase str

--- script.c
+++ script.c
@@ -5,33 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: data_kernel <data_kernel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/16 16:52:13 by data_kernel       #+#    #+#             */
-/*   Updated: 2024/08/17 19:09:52 by data_kernel      ###   ########.fr       */
+/*   Created: 2024/08/17 19:14:47 by data_kernel       #+#    #+#             */
+/*   Updated: 2024/08/17 19:22:36 by data_kernel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <string.h>
 
-int		ft_strncmp(char *, char *, unsigned int);
-
-int		ft_strncmp(char *s1, char *s2, unsigned int n)
+char	*ft_strupcase(char *str)
 {
 	int		i = 0;
 
-	while (i < n && (s1[i] || s2[i]))
+	while(str[i])
 	{
-		//debuging
-		//printf("iterations:%i___", i);
-		if(s1[i] < s2[i] || s1[i] > s2[i])
-			return(s1[i] - s2[i]);
+		if(str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 		i++;
 	}
-	return(0);
+
+	return(str);
 }
 
 int		main(void)
 {
-	printf("%i", ft_strncmp("ab", "ab", 10));
+	char	str[] = "L$ncelot";
+	
+	ft_strupcase(str);
+	printf("%s", str);
 	return(0);
 }
