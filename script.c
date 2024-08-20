13c59244a520b76a4cedca292bfc36974dd0717a
dataKernel <lancelot.saintleger@gmail.com>
2024-08-20 17:15:29 +0100

    capitalizer

--- script.c
+++ script.c
@@ -1,37 +1,58 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   script.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: data_kernel <data_kernel@student.42.fr>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/17 19:14:47 by data_kernel       #+#    #+#             */
-/*   Updated: 2024/08/17 19:22:36 by data_kernel      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
-#include <string.h>
 
-char	*ft_strupcase(char *str)
+
+void 	ft_strlowcase(char *str)
 {
-	int		i = 0;
+	int		i;
 
+	i = 0;
 	while(str[i])
 	{
-		if(str[i] >= 'a' && str[i] <= 'z')
-			str[i] -= 32;
+		if(str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
 		i++;
 	}
+}
 
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return(i);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int		i;
+
+	i = 1;
+	//on passe le string en full min
+	ft_strlowcase(str);
+	if(str[0] >= 'a' || str[0] <= 'z')
+		str[0] -= 32;
+		
+	while(str[i] && ft_strlen(str) > 1)
+	{
+		if((str[i] >= 'a' && str[i] <= 'z'))
+		{
+			if(str[i - 1] < '0' || (str[i - 1] > '9' 
+			&& str[i - 1] < 'A') || (str[i - 1] > 'Z' 
+			&& str[i] < 'a'))
+				str[i] -= 32;
+		}
+		i++;
+	}
 	return(str);
 }
 
 int		main(void)
 {
-	char	str[] = "L$ncelot";
-	
-	ft_strupcase(str);
-	printf("%s", str);
+	char	test[] = "test, du truc? 42ok ca-pUUt?";
+
+	ft_strcapitalize(test);
+	printf("%s", test);
 	return(0);
 }
