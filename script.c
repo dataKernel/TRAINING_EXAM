725d323d731e8c11d4c2297f7ca6f8865fd956a0
dataKernel <lancelot.saintleger@gmail.com>
2024-08-21 11:05:42 +0100

    str_is_alpha

--- script.c
+++ script.c
@@ -1,58 +1,24 @@
 #include <stdio.h>
 
-
-void 	ft_strlowcase(char *str)
+int		ft_str_is_alpha(char *str)
 {
 	int		i;
 
+	if(str == NULL)
+		return(1);
 	i = 0;
 	while(str[i])
 	{
-		if(str[i] >= 'A' && str[i] <= 'Z')
-			str[i] += 32;
-		i++;
+		if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] >= 'Z'))
+			i++;
+		else
+			return(0);
 	}
-}
-
-int		ft_strlen(char *str)
-{
-	int		i;
-
-	i = 0;
-	while(str[i])
-		i++;
-	return(i);
-}
-
-char	*ft_strcapitalize(char *str)
-{
-	int		i;
-
-	i = 1;
-	//on passe le string en full min
-	ft_strlowcase(str);
-	if(str[0] >= 'a' || str[0] <= 'z')
-		str[0] -= 32;
-		
-	while(str[i] && ft_strlen(str) > 1)
-	{
-		if((str[i] >= 'a' && str[i] <= 'z'))
-		{
-			if(str[i - 1] < '0' || (str[i - 1] > '9' 
-			&& str[i - 1] < 'A') || (str[i - 1] > 'Z' 
-			&& str[i] < 'a'))
-				str[i] -= 32;
-		}
-		i++;
-	}
-	return(str);
+	return(1);
 }
 
 int		main(void)
 {
-	char	test[] = "test, du truc? 42ok ca-pUUt?";
-
-	ft_strcapitalize(test);
-	printf("%s", test);
+	printf("%i", ft_str_is_alpha("sqmdlkfjqs>dmlkfj"));
 	return(0);
 }
