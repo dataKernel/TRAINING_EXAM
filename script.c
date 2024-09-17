b38245893443545d8a4c4e78226bf4d683230c89
dataKernel <lancelot.saintleger@gmail.com>
2024-09-17 11:05:30 +0100

    argv ascii ordering exe

--- script.c
+++ script.c
@@ -1,5 +1,7 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
+//just in case
+#include <stdbool.h>
 
 void	ft_putchar(char c)
 {
@@ -18,16 +20,44 @@ void	ft_putstr(char *str)
 	}
 }
 
+int		str_cmp_by_ascii(char *str1, char *str2)
+{
+	int		i;
+
+	i = 0;
+	while((str1[i] || str2[i]) && str1[i] == str2[i])
+		i++;
+	return(str1[i] - str2[i]);
+}
+
+
 int		main(int ac, char *av[])
 {
-	int		indexArgsCount;
+	char	*temp;
+	int		i;
+	int		j;
 
-	indexArgsCount = 1;
-
-	while(indexArgsCount < ac)
+	temp = NULL;
+	i = 1;
+	while(i < ac)
 	{
-		ft_putstr(av[ac - indexArgsCount]);
-		indexArgsCount++;
+		j = i + 1;
+		while(j < ac)
+		{
+			if(str_cmp_by_ascii(av[i], av[j]) > 0)
+			{
+				temp = av[i];
+				av[i] = av[j];
+				av[j] = temp; 
+			}
+			j++;
+		}
+		i++;
+	}
+
+	for(int i = 0; i < ac; i++)
+	{
+		printf("args[%i]:%s\n", i, av[i]);
 	}
 	return(0);
 }
