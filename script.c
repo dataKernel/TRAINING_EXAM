3c84470a6036c2e9cc58f5662095023a8d89b927
dataKernel <lancelot.saintleger@gmail.com>
2024-10-10 14:59:10 +0100

    setup ultimate

--- script.c
+++ script.c
@@ -1,55 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#define	MIN		1
+#define MIN		0
 #define MAX		5
 
-int		ft_strlen(char *str)
+int		ft_ultimate_range(int **range, int min, int max)
 {
-	int		i;
-
-	i = 0;
-	while(str[i])
-		i++;
-	return(i);
-}
-
-int		*ft_range(int min, int max)
-{
-	int		*ptr;
-	int		sizeArray;
-	int		i;
-
-	if(min >= max)
-		return(NULL);
-	ptr = malloc(sizeof(int) * (max - min));
-	if(ptr == NULL)
-		return(NULL);
-	sizeArray = max - min;
-	i = 0;
-	while(i < sizeArray)
-	{
-		ptr[i] = min;
-		min++;
-		i++;
-	}
-	return(ptr);
+	return(0);
 }
 
 int		main(void)
 {
-	int		*ptrlocal;
+	int		*tabInt;
 
-	ptrlocal = ft_range(MIN, MAX);
-	if(!ptrlocal)
-	{
-		printf("ptr local was NULL");
-		return(-1);
-	}
-	for(int i = 0; i < MAX - MIN; i++)
-	{
-		printf("ptrlocal[%i]:%i\n", i, ptrlocal[i]);
-	}
-	free(ptrlocal);
+	tabInt = ft_ultimate_range(tabInt, MIN, MAX);
 	return(0);
 }
