bcc9f88f350b4a3410f572d02dc0c2506cf3b059
dataKernel <lancelot.saintleger@gmail.com>
2024-10-27 16:07:22 +0000

    updating malloc protec/main comment/debug size and indexation on malloc allocation and fill

--- script.c
+++ script.c
@@ -1,65 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int     ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int     i;
+	int	i;
 
-    i = 0;
-    while(str[i])
-        i++;
-    return(i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+void	ft_copy_str(char *src, char *dst)
+{
+	int	i;
+
+	i = 0;
+	if(!src || !dst)
+		return;
+	while (src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
 }
 
-void    ft_copy_str(char *src, char *dst)
+char	*ft_concat_params(int argc, char **argv)
 {
-    int     i;
+	char	*tabMalloc;
+	int		sizeTab;
+	int		indexArgc;
+	int		indexArgv;
 
-    i = 0;
-    while(src[i])
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    dst[i] = '\n'; // on ajoute un retour chariot en fin de copie pr directement l'avoir pr malloc
+	tabMalloc = NULL;
+	sizeTab = 0;
+	indexArgc = 1;
+	indexArgv = 0;
+	// calcul du tab malloc
+	while (indexArgc < argc)
+	{
+		sizeTab += ft_strlen(argv[indexArgc]);
+		indexArgc++;
+	}
+	tabMalloc = malloc(sizeof(char) * (sizeTab + argc - 1));
+	if (!tabMalloc) // protect malloc
+		return (NULL);
+	indexArgc = 1;
+	indexArgv = 0;
+	while (indexArgc < argc)
+	{
+		ft_copy_str(argv[indexArgc], tabMalloc + indexArgv);
+		indexArgv += ft_strlen(argv[indexArgc]);
+		if (indexArgc < argc - 1)
+		{
+			tabMalloc[indexArgv] = '\n';
+			indexArgv++;
+		}
+		indexArgc++;
+	}
+	tabMalloc[indexArgv] = '\0';
+	return (tabMalloc);
 }
 
-char    *ft_concat_params(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    char    *tabMalloc;
-    int     sizeTab;
-    int     indexArgc;
-    int     indexArgv;
+	char *tab = ft_concat_params(argc, argv);
 
-    tabMalloc = NULL;
-    sizeTab = 0;   
-    indexArgc = 1;
-    indexArgv = 0;
-    while(indexArgc < argc)
-    {
-        sizeTab += ft_strlen(argv[indexArgc]);
-        indexArgc++;
-    }
-    printf("checking de la size: %i", sizeTab + argc-1);
-    tabMalloc = malloc(sizeof(char) * (sizeTab + argc - 1));
-    indexArgc = 1;
-    indexArgv = 0;
-    while(indexArgc < argc)
-    {
-        ft_copy_str(argv[indexArgc], tabMalloc + indexArgv);
-        indexArgv += ft_strlen(argv[indexArgc]) + 1;
-        indexArgc++;
-    }
-    tabMalloc[indexArgv] = '\0';
-    return(tabMalloc);
-}
-
-int     main(int argc, char **argv)
-{
-    char    *tab = ft_concat_params(argc, argv);
-
-
-    printf("malloc assignation: %s", tab);
-    free(tab);
-    return(0);
+	if (!tab)
+		return (-1); // erreur d'allocation
+	//checking output
+	printf("malloc assignation: \n%s", tab);
+	free(tab);
+	return (0);
 }
