7a482bafc65a988765c369c04854e1885b32941c
dataKernel <lancelot.saintleger@gmail.com>
2024-11-29 16:12:24 +0000

    (+):concat params algo with malloc (+): need to add str cpy function to reduce main func size

--- script.c
+++ script.c
@@ -1,64 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
-//--------------protos---------------
-char	*ft_concat(int, char *[]);
-//-----------------------------------
+
 int		ft_strlen(char *str)
 {
 	int		i = 0;
 
 	while(str[i])
 		i++;
+
 	return(i);
 }
 
-char	*ft_concat(int ac, char *av[])
+int		checking_size_str_argv(int argsCount, char *argvTab[])
+{
+	int		i = 1;//index argc
+	int		res = 0;
+	
+	while(i < argsCount)
+	{
+		res += ft_strlen(argvTab[i]);
+		i++;
+	}
+	//ajout des espaces et du NUL terminator
+	res += (argsCount - 2) + 1;
+	
+	return(res);
+}
+
+char	*ft_concat_params(int argc, char *argv[])
 {
 	char	*mallocStr = NULL;
-	int		sizeStr = 0;
-	int		indexMallocStr = 0;
-	int		i = 1;//index ac, skipping the first one
-	//(executable program)
-	int		j;//index av
-	
-	//calc full size str
-	while(i < ac)
-	{
-		sizeStr += ft_strlen(av[i]);
-		i++;
-	}
-	//we add the \n and NUL terminator
-	sizeStr += ac - 2 + 1; //could resume with ac - 1
-	mallocStr = malloc(sizeof(char) * sizeStr);
+	int		sizeMalloc = checking_size_str_argv(argc, argv);
+	int		i = 1; //index de l'argv sur le 1er argument (on skip le nom du prog)
+	mallocStr = malloc(sizeof(char) * sizeMalloc);
+	//on check la gen dynamique de malloc
 	if(!mallocStr)
-	{
-		printf("fail allocation");
 		return(NULL);
-	}
-	//filling the dynamical alloc
-	i = 1;
-	while(i < ac)
+	while(i < argc)
 	{
 		j = 0;
-		while(av[i][j])
+		while(argv[i][j])
 		{
-			mallocStr[indexMallocStr] = av[i][j];
-			indexMallocStr++;
+			mallocStr[j] = argv[i][j];
 			j++;
 		}
-		if(i != ac - 1)
-			mallocStr[indexMallocStr++] = '\n';
+		if(i < argc - 1)
+			mallocStr[j] = '\n';
 		i++;
 	}
-	mallocStr[indexMallocStr++] = '\0'; 
 	return(mallocStr);
 }
 
 int		main(int argc, char *argv[])
 {
-	char	*str = ft_concat(argc, argv);
-
-	printf("%s", str);
+	char	*tst;
+	tst = ft_concat_params(argc, argv);
+	
+	printf("%s", tst);
 	return(0);
 }
-
