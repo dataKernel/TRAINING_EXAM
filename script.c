7a8f5cf3f353e9e1986ef7ed6b6e355a0d61196e
dataKernel <lancelot.saintleger@gmail.com>
2025-04-16 12:58:51 +0100

    revision algo exam

--- script.c
+++ script.c
@@ -1,38 +1,65 @@
+/*
+Assignment name  : rot_13
+Expected files   : rot_13.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and displays it, replacing each of its
+letters by the letter 13 spaces ahead in alphabetical order.
+
+'z' becomes 'm' and 'Z' becomes 'M'. Case remains unaffected.
+
+The output will be followed by a newline.
+
+If the number of arguments is not 1, the program displays a newline.
+
+Example:
+
+$>./rot_13 "abc"
+nop
+$>./rot_13 "My horse is Amazing." | cat -e
+Zl ubefr vf Nznmvat.$
+$>./rot_13 "AkjhZ zLKIJz , 23y " | cat -e
+NxwuM mYXVWm , 23l $
+$>./rot_13 | cat -e
+$
+$>
+$>./rot_13 "" | cat -e
+$
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 
-#define AM      "A.M."
-#define PM      "P.M."
+#define DIST    13
 
-void    ft_aura_lieu(int);
-
-void    ft_aura_lieu(int heure)
+void    ft_putchar(char c)
 {
-    //check param
-    if(heure < 0 || heure > 23)
-    {
-        printf("Erreur de param..");
-        return;
-    }
-    if(heure == 11)
-        printf("%i:00 %s -> %i:00 %s\n", heure, AM, heure + 1, PM);
-    else if(heure == 23)
-        printf("%i:00 %s -> %i:00 %s\n", heure - 12, PM, heure + 1 - 12, AM);
-    else if(heure < 12)
-    {
-            printf("%i:00 %s -> %i:00 %s\n", heure, AM, heure + 1, AM);
-    }
-    else if(heure >= 12)
-    {
-        if(heure == 12)
-            printf("%i:00 %s -> %i:00 %s\n", heure, PM, heure + 1 - 12, PM);
-        else
-            printf("%i:00 %s -> %i:00 %s\n", heure - 12, PM, heure + 1 - 12, PM);
-    }
+    write(1, &c, 1);
 }
 
-int     main(void)
+int     main(int argc, char *argv[])
 {
-    ft_aura_lieu(11);
+    char    *str = argv[1];
+    char    letter;
+
+    if(argc != 2)
+    {
+        printf("\n");
+        return(0);
+    }
+
+    for(int i = 0; str[i]; i++)
+    {
+        letter = str[i];
+        for(int counter = 0; counter < DIST; counter++)
+        {
+            if(letter == 'z' || letter == 'Z')
+                letter = 'a';
+            else if(letter >= 'a' && letter <= 'z' || letter >= 'A' && letter <= 'Z')
+                letter++;
+        }
+        ft_putchar(letter);
+    }
     return(0);
 }
